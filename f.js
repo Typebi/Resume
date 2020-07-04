b9292220da03b3e8f4b3b88ff0dfe3c0994da9ac
psydrate <exprain2001@naver.com>
2020-07-05 01:28:31 +0900

    Update contents and images

--- f.js
+++ f.js
@@ -1,17 +1,17 @@
-function showTOEIC(ans, objName) {
+function showImg(ans, objName) {
 	var obj = document.getElementById(objName); 
-	if(ans) { 
-		obj.style.top = window.event.clientY + document.body.scrollTop + 10; 
-		obj.style.left = window.event.clientX + document.body.scrollLeft + 10; 
-		obj.style.display = "block"; 
-	}else { 
-		obj.style.display = "none"; 
-	}
+	obj.style.display = ans && "block" || "none";
 }
-function moveTOEIC(objName) {
+function moveImg(objName) {
 	var obj = document.getElementById(objName); 
 	if (obj.style.display == "block") {
-		obj.style.top = window.event.clientY + document.body.scrollTop + 10; 
-		obj.style.left = window.event.clientX + document.body.scrollLeft + 10;
+		obj.style.left = (event.clientX * 0.8) + document.documentElement.scrollLeft + "px";
+		obj.style.top = event.clientY + document.documentElement.scrollTop -150 + "px";
 	}
+}
+function mouseOver(ans, obj1, obj2) {
+	var obj1 = document.getElementById(obj1);
+	var obj2 = document.getElementById(obj2);
+	obj1.style.display = ans && "block" || "none";
+	obj2.style.display = ans && "none" || "block";
 }
